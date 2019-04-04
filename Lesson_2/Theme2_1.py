1f8f80a15b96fc5be0192e10048a252cf669d89a
Igor <tsyglintsev-iv@ural.rt.ru>
2019-04-04 23:46:39 +0500

    ура, получилось наконец

--- Lesson_2/Theme2_1.py
+++ Lesson_2/Theme2_1.py
@@ -6,3 +6,20 @@
 my_list_1 = [2, 5, 8, 2, 12, 12, 4]
 my_list_2 = [2, 7, 12, 3]
 
+r = len(my_list_2)
+
+
+for x in range(r):
+    print ("my_list_2", "[x]" "=", my_list_2[x])
+
+    i=0
+    while i < len(my_list_1):
+        if (my_list_1[i] == my_list_2[x]):
+#            del my_list_1[i]
+            my_list_1.pop(i)
+        else:
+            i=i+1
+
+print(my_list_1)
+
+
