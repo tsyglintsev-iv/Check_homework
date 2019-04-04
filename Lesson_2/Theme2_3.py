afd2e1b949d93ae439bc63ef4c016c78403ef183
Igor <tsyglintsev-iv@ural.rt.ru>
2019-04-04 22:14:27 +0500

    create

---
+++ Lesson_2/Theme2_3.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+#Дан список заполненный произвольными целыми числами.
+
+
+my_list_1 = [2, 2, 5, 12, 8, 2, 12]
+
+my_list = set(my_list_1)
+
+print (my_list)
