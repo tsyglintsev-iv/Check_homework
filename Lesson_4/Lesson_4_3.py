53e96f8cd042a0600230c0a9d8e86be16b94b5ca
Igor <tsyglintsev-iv@ural.rt.ru>
2019-04-06 22:21:05 +0500

    in progress

--- Lesson_4/Lesson_4_3.py
+++ Lesson_4/Lesson_4_3.py
@@ -1,9 +1,25 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-#3.Давайте опишем пару сущностей player и enemy через словарь, который будет иметь ключи и значения:
+# 3.Давайте опишем пару сущностей player и enemy через словарь, который будет иметь ключи и значения:
 #    name — строка, полученная от пользователя,
 #    health = 100,
 #    damage = 50.
 
 
+users = {
+    "player": {
+        "name": "Maxik",
+        "health": 100,
+        "damage" : 50
+    },
+    "enemy": {
+        "name": "Boris",
+        "health": 100,
+        "skype": 50
+    }
+}
+
+
+
+print(users["player"])
