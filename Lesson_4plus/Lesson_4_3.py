c06dd86bbc59dac42532718b8ddb57e5c2340901
Igor <tsyglintsev-iv@ural.rt.ru>
2019-04-07 13:16:44 +0500

    commit

---
+++ Lesson_4plus/Lesson_4_3.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+# 3.Давайте опишем пару сущностей player и enemy через словарь, который будет иметь ключи и значения:
+#    name — строка, полученная от пользователя,
+#    health = 100,
+#    damage = 50.
+
+player = "Кузьма"
+enemy = "Борис"
+
+users = {
+    player: {
+        "name": player,
+        "health": 1000,
+        "damage": 50
+    },
+    enemy: {
+        "name": enemy,
+        "health": 100,
+        "damage": 50
+    }
+}
+
+
+def fight(attacker, defender):
+    defender["health"] = (defender["health"] - attacker["damage"])
+    print('{} атаковал {} и оставил ему {} здоровья'.format(attacker['name'], defender['name'],
+                                                                      defender['health']))
+    # print(defender["health"])
+
+
+fight(users[player], users[enemy])
+# print(users)
+fight(users[enemy], users[player])
+# print(users)
---
+++ Lesson_4plus/Lesson_4_4.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+# 3.Давайте опишем пару сущностей player и enemy через словарь, который будет иметь ключи и значения:
+#    name — строка, полученная от пользователя,
+#    health = 100, здоровье сущности
+#    damage = 50, урон, уоторый нанносит сущность
+#    armor  = 1.2 коэффициент снижения урона от врага
+
+player = "Кузьма"
+enemy = "Борис"
+
+users = {
+    player: {
+        "name": player,
+        "health": 1000,
+        "damage": 50,
+        "armor": 1.2
+    },
+    enemy: {
+        "name": enemy,
+        "health": 100,
+        "damage": 50,
+        "armor" : 1.99
+    }
+}
+#Вычисляем урон с учётом брони
+def armoring (damage, armor):
+    return (damage / armor)
+
+#Вычисляем оставшееся здоровье сущности
+def fight(attacker, defender):
+    defender["health"] = round (defender["health"] - armoring(attacker["damage"], defender["armor"]) )
+    print('{} атаковал {} и оставил ему {} здоровья'.format(attacker['name'], defender['name'], defender['health']))
+
+
+fight(users[player], users[enemy]) #Игрок бьёт по врагу
+fight(users[enemy], users[player]) #Вражина отвечает
