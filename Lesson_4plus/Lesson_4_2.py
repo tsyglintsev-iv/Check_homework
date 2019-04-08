5cd54e44ce84870a81f0084f904e4939f08e32ab
Igor <tsyglintsev-iv@ural.rt.ru>
2019-04-08 22:18:27 +0500

    Переделал. Теперь фунция ищет наибольшее значение из трёх в цикле.

--- Lesson_4plus/Lesson_4_2.py
+++ Lesson_4plus/Lesson_4_2.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-
 # 2. Создайте функцию, принимающую на вход 3 числа и возвращающую наибольшее из них.
 
-
 def maxit(num1, num2, num3):    #Наша функция
-
     numbers = [num1, num2, num3]
-    print(f"Наибольшее число из трёх - {max(numbers)}")
+    max=0
+    for x in numbers:
+        print(x)
+        if x>max:
+            max = x
 
+    return max
 
-maxit(309,5,30)         # Передаём значения функции
+print(f"Наибольшее число из трёх - {maxit(309,5,30)}")
